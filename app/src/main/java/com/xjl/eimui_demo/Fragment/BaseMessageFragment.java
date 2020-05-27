4f1c635f10d29ff1842190fafb97d56504a80f72
许金龙 <67765544@qq.com>
2020-05-27 09:23:37 +0800

    去掉参考 部分代码。

--- app/src/main/java/com/xjl/eimui_demo/Fragment/BaseMessageFragment.java
+++ app/src/main/java/com/xjl/eimui_demo/Fragment/BaseMessageFragment.java
@@ -33,7 +33,6 @@ public abstract class BaseMessageFragment extends Fragment {
     public IUser mine, other;
     public int messageId = 0;
 
-    public final String longText = "布鲁斯·韦恩（Bruce Wayne）即蝙蝠侠（Batman），是美国DC漫画旗下超级英雄，初次登场于《侦探漫画》（Detective Comics）第27期（1939年5月），由鲍勃·凯恩（Bob Kane）和比尔·芬格（Bill Finger）联合创造，是漫画史上第一位没有超能力的超级英雄。布鲁斯·韦恩出生在哥谭市四大家族之一的韦恩家族中。一天晚上，父母带着年幼的布鲁斯看完电影《佐罗》回家，途经一条小径时遭遇歹徒的抢劫。歹徒当着布鲁斯的面枪杀了他的父母。从此，布鲁斯便产生了亲手铲除罪恶的强烈愿望，为了不让其他人再遭受到与自己同样的悲剧，凭借着过人的天赋，布鲁斯利用十几年时间游历世界各地，拜访东西方顶级或传说中的格斗大师，学习各流派格斗术，后回到美国，利用强大的财力制造各种高科技装备。此后在白天，他是别人眼中的无脑富二代、花花公子；夜晚，他是令罪犯闻风丧胆的黑暗骑士——蝙蝠侠（Batman）。";
 
     public final String mineAvatar = "http://img3.imgtn.bdimg.com/it/u=276120026,1236011605&fm=26&gp=0.jpg";
     public final String otherAvatar = "http://imgsrc.baidu.com/forum/w%3d580/sign=af65a417e6cd7b89e96c3a8b3f254291/87e498cad1c8a786b275eed36709c93d72cf508f.jpg";
--- chatinput/src/main/java/cn/jiguang/imui/chatinput/listener/CameraControllerListener.java
+++
@@ -1,8 +0,0 @@
-package cn.jiguang.imui.chatinput.listener;
-
-public interface CameraControllerListener {
-    void onFullScreenClick();
-    void onRecoverScreenClick();
-    void onCloseCameraClick();
-    void onSwitchCameraModeClick(boolean isRecordVideoMode);
-}
